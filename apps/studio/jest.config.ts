d00f4916838c6ab3c683f8dba52fe2f686ab2ca8
Gustavo Perdomo <gperdomor@gmail.com>
2022-05-04 02:55:54 +0000

    chore: update to nx 14

--- apps/studio/jest.config.ts
+++ apps/studio/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'studio',
-  preset: '../../jest.preset.js',
+  preset: '../../jest.preset.ts',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
