834b02a9779be28860901d72cfc9e9bac35cfcae
Gustavo Perdomo <gperdomor@gmail.com>
2022-05-11 00:39:20 -0400

    feat(studio): initial login screens

---
+++ apps/studio/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+      <span className="block xl:inline">HOME INDEX</span>
+    </h1>
+  );
+}
